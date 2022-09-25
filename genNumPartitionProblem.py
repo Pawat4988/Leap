6e00ac80dbd3108b6e1846e60540e1f96489d567
Pawat4988 <59772273+Pawat4988@users.noreply.github.com>
2022-09-25 23:51:37 +0700

    Update genNumPartitionProblem.py

--- genNumPartitionProblem.py
+++ genNumPartitionProblem.py
@@ -4,15 +4,22 @@ import random
 genProblem = []
 
 for i in range(2):
-    eachSubsetTotal = 150
-    genNum = random.randint(1,50)
+    eachSubsetTotal = 59
+    genNum = random.randint(1,15)
     while eachSubsetTotal > genNum:
         genProblem.append(genNum)
         eachSubsetTotal -= genNum
         genNum = random.randint(1,50)
-    if genNum > 0:
-        genProblem.append(genNum)
+    if eachSubsetTotal > 0:
+        genProblem.append(eachSubsetTotal)
         
 print(genProblem)
+
+total = 0
+test = genProblem
+for num in test:
+    total+=num
+
+print(total)
 
 
