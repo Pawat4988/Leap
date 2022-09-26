cfc06beb9fa10253483c87a0337b0ca27bffbe66
Pawat4988 <59772273+Pawat4988@users.noreply.github.com>
2022-09-26 23:46:40 +0700

    Update cqm.py

--- cqm.py
+++ cqm.py
@@ -1,19 +1,37 @@
-from dwave.system import LeapHybridCQMSampler
+from dwave.system import LeapHybridCQMSampler, LeapHybridSampler
 from dimod import ConstrainedQuadraticModel, Integer
+from dimod import cqm_to_bqm
+from dimod.binary import BinaryQuadraticModel
+import dimod
+import json
 
-i = Integer('i', upper_bound=25)
-j = Integer('j', upper_bound=25)
+i = Integer('i', upper_bound=100)
+j = Integer('j', upper_bound=100)
 
 cqm = ConstrainedQuadraticModel()
-cqm.set_objective(-(i+j))
+cqm.set_objective(-i*j)
 
-cqm.add_constraint(i*j <= 25, "Max perimeter")
+cqm.add_constraint(2*i+2*j <= 32, "Max perimeter")
 
+# sampler = LeapHybridCQMSampler()
+sampler2 = LeapHybridSampler()
 
-sampler = LeapHybridCQMSampler()                
+bqm, invert = cqm_to_bqm(cqm)
+Q = BinaryQuadraticModel.to_qubo(bqm)
+# print(bqm)
 
-sampleset = sampler.sample_cqm(cqm)             
+# response = sampler.sample_cqm(cqm)
+response2 = sampler2.sample(bqm)
 
-# print(sampleset.first)  
-for sample, energy in sampleset.data(fields=['sample','energy']):
+newinvert = dimod.constrained.CQMToBQMInverter.from_dict(
+    json.loads(json.dumps(invert.to_dict())))
+
+for sample, energy in response2.data(fields=['sample','energy']):
     print(sample,energy)
+
+# for sample, energy in response2.data(fields=['sample','energy']):
+#     print(newinvert(sample),energy)  
+
+# invert(response2.first.sample)   
+
+print(newinvert(response2.first.sample))
