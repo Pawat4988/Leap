77d809151ef92876d8a52147127eca4286ff45a1
Pawat4988 <59772273+Pawat4988@users.noreply.github.com>
2022-09-27 23:07:12 +0700

    commit

--- test.py
+++ test.py
@@ -1,37 +1,19 @@
-import dwave.inspector
-import time
-from collections import defaultdict
+i = 110
+j = 21
+k = 1
+if (5*i+10*j <= 850):
+    print("pass")
+else:
+    print("fail")
 
-from dwave.system.samplers import DWaveSampler
-from dwave.system.composites import EmbeddingComposite
-import networkx as nx
+if (i+j >= 95):
+    print("pass")
+else:
+    print("fail")
 
-import matplotlib
-matplotlib.use("agg")
-from matplotlib import pyplot as plt
+if (30*i+20*j <= 2700):
+    print("pass")
+else:
+    print("fail")
 
-# ------- Set up our graph -------
-
-# Create empty graph
-G = nx.Graph()
-
-# Add edges to the graph (also adds nodes)
-G.add_edges_from([(1,2),(1,3),(2,4),(3,4),(3,5),(4,5)])
-
-# ------- Set up our QUBO dictionary -------
-
-# Initialize our Q matrix
-Q = defaultdict(int)
-
-
-
-print(Q)
-print(Q[1])
-# Update Q matrix for every edge in the graph
-for i, j in G.edges:
-    Q[(i,i)]+= -1
-    Q[(j,j)]+= -1
-    Q[(i,j)]+= 2
-
-    print(Q)
-    time.sleep(5)
+print(10*i+20*j+30*k)
