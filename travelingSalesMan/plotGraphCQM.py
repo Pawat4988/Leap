f7a09599e1fac5c822b7eb359cb1871ea5375062
Pawat Fongasira <pawatdon2543@gmail.com>
2022-11-01 09:57:04 +0000

    cqm plot

--- travelingSalesMan/plotGraphCQM.py
+++ travelingSalesMan/plotGraphCQM.py
@@ -10,14 +10,22 @@ from matplotlib import pyplot as plt
 # plt.plot(x, y,"ro")
 # plt.show()
 
-# x = np.array([5,5,5,10,10,10,20,20,20,30,30,30,40,40,40,60,60,60])
-# y = np.array([-584, -936, -1000,-815, -817, -671,-724, -892, -638,-379, -570, -660,-643, -393, -725,-614,-516,-480])
-# plt.plot(x, y,"ro")
-# plt.show()
 
-x = np.array([5,10,20,30,40,60])
-y = np.array([(-584-936-1000)/3,(-815-817-671)/3,(-724-892-638)/3,(-379-570-660)/3,(-643-393-725)/3,(-614-516-480)/3])
-# my_xticks = [5,10,20,30,40,60]
-# plt.xticks(x, my_xticks)
+
+solverName = "cqm"
+problemName = "gr17"
+
+bestAnswerErrors = [-584, -936, -1000,-815, -817, -671,-724, -892, -638,-379, -570, -660,-643, -393, -725,-614,-516,-480]
+x = np.array([5,5,5,10,10,10,20,20,20,30,30,30,40,40,40,60,60,60])
+y = np.array(bestAnswerErrors)
 plt.plot(x, y,"ro")
 plt.show()
+plt.savefig(f'travelingSalesMan/graph/{solverName}_{problemName}Plot.png')
+plt.clf()
+
+x = np.array([5,10,20,30,40,60])
+mean = [(bestAnswerErrors[(i*3)]+bestAnswerErrors[(i*3+1)]+bestAnswerErrors[(i*3+2)])/3 for i in range(6)]
+y = np.array(mean)
+plt.plot(x, y,"ro")
+plt.show()
+plt.savefig(f'travelingSalesMan/graph/{solverName}_{problemName}MeanPlot.png')
